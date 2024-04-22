aa684366364289bae2ca0fe60333c80d29ca1683
masonhyz <mason.hu@mail.utoronto.ca>
2024-04-22 16:30:27 -0400

    uncommitted files not for flask deployment

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import json
 import plotly
 from plots import *
 
-
 app = Flask(__name__)
 
 graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
 
-
 @app.route("/")
 def index():
     return render_template("index.html", title = "Home", graph1JSON = graph1JSON)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
